370c8683204cbea107838bce05dc52935f09bb91
guru2402 <prasadguru141@gmail.com>
2017-05-13 20:19:29 +0530

    Movie search

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import SearchBar from './containers/search_bar.js';
 
 import App from './components/app';
 import reducers from './reducers';
